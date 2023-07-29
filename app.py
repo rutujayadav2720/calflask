4c2a7e4b31660689f2166fb46a3cb36e01d1548d
Rutuja Yadav <[valid-email]>
2023-07-29 10:25:13 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask,request,render_template 
+
+
+obj=Flask(__name__)
+
+
+@obj.route('/')
+def welcome():
+    return "Welcome to flask"
+
+@obj.route('cal',methods=["GET"])
+def math_operator():
+    Operation=request.json["operation"]
+    number1=request.json["number1"]
+    number2=request.json["number2"]
+    
+if operation=="add":
+     result=number1+number2
+elif operation=="multiply":
+     result=number1*number2   
+elif operation=="division":
+     result=number1/number2
+else
+     result=number1-number2
+return result
+
+
+
+print(__name__)
+
+if__name__ == '__main__':
+    obj.run()
+
